a02c4b7e8573e744814a84afb70b7b6922887c9e
Jaeyoung Choi <hideyf@gmail.com>
2017-02-14 09:17:38 +0900

    add project

---
+++ IUExtensions/IUExtensions.h
@@ -0,0 +1,19 @@
+//
+//  IUExtensions.h
+//  IUExtensions
+//
+//  Created by ricky on 2017. 2. 14..
+//  Copyright © 2017년 Appcid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IUExtensions.
+FOUNDATION_EXPORT double IUExtensionsVersionNumber;
+
+//! Project version string for IUExtensions.
+FOUNDATION_EXPORT const unsigned char IUExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IUExtensions/PublicHeader.h>
+
+
